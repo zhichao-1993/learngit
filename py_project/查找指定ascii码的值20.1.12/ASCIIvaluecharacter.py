745e494e3328bdc6da23663b2dbc03b7600f7d57
zhichao <1522554132@qq.com>
2021-01-12 16:03:26 +0800

    修改格式

--- py_project/查找指定ascii码的值20.1.12/ASCIIvaluecharacter.py
+++ py_project/查找指定ascii码的值20.1.12/ASCIIvaluecharacter.py
@@ -3,7 +3,7 @@
 """
 # 1.输入需要查找的字符
 c = input("输入要查找的字符：")  # 1.input()函数，返回string类型
-# 2.查找指定的字符的ASCII值 # 1. ord()函数，返回字符对应的ASCII值
-asc = ord(c)
+# 2.查找指定的字符的ASCII值
+asc = ord(c)  # 1. ord()函数，返回字符对应的ASCII值
 # 3.打印ASCII值
 print("{0}的ASCII值是{1}".format(c, asc))  # 1.print()函数 2.format()函数

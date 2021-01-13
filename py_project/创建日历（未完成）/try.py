c84e841459d3e408d0981f68843bc5299f044b1b
zhichao <1522554132@qq.com>
2021-01-13 22:13:00 +0800

    add try file

---
+++ py_project/创建日历（未完成）/try.py
@@ -0,0 +1,52 @@
+from tkinter import *  # 导入thinter模块中的所有类   thinter是python中处理Gui的模块
+import calendar        # 导入calendar整个模块               calendar是python的处理日历的模块
+# from tkinter import * 和 import tkinter 有什么区别？以下是例子
+#    import Tkinter            root = Tkinter.Tk()
+#    from Tkinter import *     root = Tk()
+
+root = Tk()  # root = Tk()，root.mainloop() 创建了以Gui窗口
+
+# root.geometry("400x300")
+root.title("Calendar")  # .title("窗口标题")方法   给窗口取一个标题
+
+# Function
+def text():
+ month_int = int(month.get())  # ? month.get() 有什么作用
+ year_int = int(year.get())    # ? year.get() 有什么作用
+ cal = calendar.month(year_int, month_int)  # calendar.month（year，month）方法，返回给定年份、月份的日历
+ textfield.delete(0.0, END)     # ？这行的作用
+ textfield.insert(INSERT, cal)  # ？这行的作用
+
+
+# Creating Labels 创建标签
+label1 = Label(root, text="Month:")  # w = Label ( 框架的父容器, 可选参数16种,text设置文本 )，Label是tkinter的标签控件
+label1.grid(row=0, column=0)  # grid()是tkinter的几何管理，管理网格；row行，column列
+
+label2 = Label(root, text="Year:")  # w = Label ( 框架的父容器, 可选参数16种,text设置文本 )，Label是tkinter的标签控件
+label2.grid(row=0, column=1)  # grid()是tkinter的几何管理，管理网格；row行，column列
+
+# Creating spinbox
+# 创建输入控件，可以指定输入的范围值
+month = Spinbox(root, from_=1, to=12, width=8)
+# w   = Spinbox(框架的父容器, 从1到12，宽度为8 )
+month.grid(row=1, column=0, padx=5)
+# padx是外部间隔  ipadx是内部间隔
+
+year = Spinbox(root, from_=2000, to=2100, width=10)
+# w   = Spinbox(框架的父容器, 从2000到2100，宽度为8 )
+year.grid(row=1, column=1, padx=10)
+# padx是外部间隔  ipadx是内部间隔
+
+# Creating Button
+#创建按钮
+button = Button(root, text="Go", command=text)  # w = Button ( 框架的父容器, text文本内容，command按钮关联的函数 )
+button.grid(row=1, column=2, padx=10)
+# 第二行的第三列，外部间隔10
+
+# Creating Textfield
+# thinter的文本控件Text
+textfield = Text(root, width=25, height=10, fg="red")  # w = Text(框架的父容器, 宽25, 高10, fg文本颜色)
+textfield.grid(row=2, columnspan=2)  # 第三行，横跨列数二
+
+
+root.mainloop()
---
+++ py_project/创建日历（未完成）/try1.py
@@ -0,0 +1,34 @@
+from tkinter import *
+import calendar
+
+# 1.创建一个Gui窗口，
+root = Tk()
+
+# 2.并给窗口去一个名字
+root.title("Calendar")
+
+# 3.添加标签
+label1 = Label(root, text="month:")
+label1.grid(row=0, column=0)
+label2 = Label(root, text="year:")
+label2.grid(row=0, column=1)
+
+# 4.创建一个输入控件Spinbox
+month = Spinbox(root, from_=1, to=12, width=7)
+month.grid(row=1, column=0, padx=5)
+year = Spinbox(root,from_=1990, to=2100, width=7)
+year.grid(row=1, column=1, padx=5)
+
+# 5.创建按钮
+button = Button(root, text="Go")  # command=text()
+button.grid(row=1, column=2, padx=10)
+
+# 6.创建空的文本框
+textfield = Text(root, width=25, height=10, fg='red')
+textfield.grid(row=2, columnspan=2)
+
+root.mainloop()
+
+# 2.获取末年某月的日历
+# cal = calendar.month(2016, 1)
+# print(cal)

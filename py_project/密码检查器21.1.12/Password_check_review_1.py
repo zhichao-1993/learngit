ffbb0c69e99f68647597ee5a84c1e184bc888679
zhichao <1522554132@qq.com>
2021-01-13 09:34:45 +0800

    add Password_check_review_1.py file

---
+++ py_project/密码检查器21.1.12/Password_check_review_1.py
@@ -0,0 +1,35 @@
+"""
+密码检查器
+1.写一个固定的密码
+2.写一个检查密码的函数
+    2.1.密码输入错误等于5次，休眠30秒，继续运行本函数  #1.if语句 2.==比较运算符 3.time。sleep()方法
+    2.2.循环5次 # 1.for循环 2.range()函数
+        2.2.1.用户输入密码 # 1.input()函数
+        2.2.2.比较 固定密码 和 用户输入密码 # 1.for循环遍历字符串的索引 2.if语句 3.==比较运算符 4.if-else语句 5.break语句
+            2.2.2.1.输出密码正确或错误 # 1.if-else语句 2.break语句
+"""
+import time
+
+pwd = 'qwer123456'
+
+def password_check():
+    count1 = 0
+    for i in range(5):
+        a = 0
+        count = 0
+        user_pwd = input("请输入密码：")
+        for j in range(len(pwd)):
+            if user_pwd[j] == pwd[a]:
+                a += 1
+                count += 1
+        if count == len(pwd):
+            print("密码正确")
+            break
+        else:
+            print("密码错误")
+            count1 += 1
+    if count1 == 5:
+        time.sleep(30)
+        password_check()
+
+password_check()

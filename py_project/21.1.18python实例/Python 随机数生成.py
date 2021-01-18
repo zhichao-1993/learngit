2676d96b51f3cac6ecba5ddf0defbe3fb8a83854
zhichao <1522554132@qq.com>
2021-01-18 16:29:44 +0800

    add 随机数

---
+++ py_project/21.1.18python实例/Python 随机数生成.py
@@ -0,0 +1,17 @@
+"""
+# -*- coding: UTF-8 -*-
+
+# Filename : test.py
+# author by : www.runoob.com
+
+# 生成 0 ~ 9 之间的随机数
+
+# 导入 random(随机数) 模块
+"""
+import random
+a =random.randint(0,9)
+if a in range(4):
+    print("选择恶臭")
+else:
+    print("选择假酒法")
+

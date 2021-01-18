fb9964e9a7db575940aa927a11675bca436d683b
zhichao <1522554132@qq.com>
2021-01-18 16:09:55 +0800

    完成阿姆斯特朗数项目

--- py_project/21.1.15阿姆斯特朗数/analysis.py
+++ py_project/21.1.15阿姆斯特朗数/analysis.py
@@ -1,14 +1,16 @@
 """
-检测三位数内的阿姆斯特朗数
+检测阿姆斯特朗数
+如果一个n位正整数等于其各位数字的n次方之和,则称该数为阿姆斯特朗数
 """
-num=int(input("enter 1-digit number:"))
+num=int(input("请输入数字:"))
 f=num
+n = len(str(num))
 sum=0
-while (f>0):
-    a=f%10
-    f=int(f/10)
-    sum=sum+(a**3)
-if(sum==num):
-    print("it is an armstrong number:",num)
+while (f>0):  # f<=0结束循环
+    a=f%10  # 取最后一位数（取模）
+    sum = sum + a ** n
+    f //= 10  # 取整除赋值运算符，c //= a 等效于 c = c // a。向下取整
+if (sum == num):
+    print("it is an armstrong number:", num)
 else:
     print("it is not an armstrong number:", num)
--- py_project/21.1.15阿姆斯特朗数/idea.py
+++ py_project/21.1.15阿姆斯特朗数/idea.py
@@ -1,3 +1,26 @@
 """
-
+检测阿姆斯特朗数
+如果一个n位正整数等于其各位数字的n次方之和,则称该数为阿姆斯特朗数
+1.输入数字
+2.获取数字长度
+3.while循环：获取每一位数
+4.输出打印
+    如果等于，输出是
+    不等于就不是
 """
+num = int(input("请输入数字:"))
+f = num
+n = len(str(num))
+sum = 0
+
+
+while (f > 0):  # f要大于0，才能执行
+    a = f%10  # 取最后一位数
+    sum = sum + a ** n
+    f //= 10  # 取整除赋值运算符，c //= a 等效于 c = c // a。把最后一位数给去掉
+
+
+if(sum == num):
+    print("it is an armstrong number:", num)
+else:
+    print("it is not an armstrong number:", num)

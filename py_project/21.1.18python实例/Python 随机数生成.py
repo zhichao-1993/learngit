e0ed94239a325eb084e5761e94fc8598e6f5cca2
zhichao <1522554132@qq.com>
2021-01-20 09:02:20 +0800

    add elif

--- py_project/21.1.18python实例/Python 随机数生成.py
+++ py_project/21.1.18python实例/Python 随机数生成.py
@@ -9,9 +9,16 @@
 # 导入 random(随机数) 模块
 """
 import random
-a =random.randint(0,9)
-if a in range(4):
+a =random.randint(1, 3)
+if a in range(1, 6):
     print("选择恶臭")
-else:
+elif a in range(6, 11):
     print("选择假酒法")
-
+elif a in range(11, 16):
+    print("选择6野兽2娜迦")
+elif a in range(16, 21):
+    print("选择6骑士2术2亡")
+elif a in range(21, 26):
+    print("选择4骑士6亡")
+elif a in range(26, 30):
+    print("选择9战士")

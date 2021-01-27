fd6eb166d64124e5ed6d5a3f2818e6f2663f4e61
zhichao <1522554132@qq.com>
2021-01-27 23:25:14 +0800

    add 提取关键字，存入字典；循环搜索

--- 歌词识曲/Lyrics_recognition.py
+++ 歌词识曲/Lyrics_recognition.py
@@ -1,17 +1,34 @@
-"""
+# 输入要查询的歌词和处理好的字典，输出歌名
+# dgbug:
+#       1.songs={}变量放到了for循环里面，每次循环都被清空了，放到循环外面就解决了
+import os
+def Get_song_keywords(path):
+#遍历filepath下所有文件，包括子目录
+    files = os.listdir(path)# 返回指定目录下的所有文件和目录名
+    songs = {}
+    for file in files:
+        file_name=os.path.join(path,file) # 路径拼接
+        with open(file_name,'r',encoding='utf-8')as f:
+            song = f.readline()
+            lyric = f.read()
+            songs[lyric] = song
+    return songs
 
-"""
-def Lyrics_recognition(file,lyric):
-    with open(file,'r',encoding='utf-8') as f:
-        list1=f.readlines()
-        list2=[]
-        for i in list1:
-            i=i.split('\n')
-            list2.append(i[0].strip())
-        if lyric in list2:
-            print(list2[0])
-        else:
-            print(False)
-file = r'小小的新娘花.txt'
-lyric = '风儿吹来了童年的一幅画'
-Lyrics_recognition(file,lyric)
+# 遍历搜索
+def search(songs):
+    ly = input("请输入歌词：")
+    count = 0
+    for lyrics in songs.keys():
+        count += 1
+        if ly in lyrics:
+            print(songs[lyrics])
+            break
+        elif count == len(songs):
+            print('没有匹配的歌词')
+
+
+if __name__ == "__main__":
+    #递归遍历目录下所有文件
+    path = 'lyric'
+    songs = Get_song_keywords(path)
+    search(songs)

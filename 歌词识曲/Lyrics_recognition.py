db42b0428aeb240c6489361c1fe6e3e440cdba9d
zhichao <1522554132@qq.com>
2021-01-28 23:59:59 +0800

    add将数据存入json,读取json

--- 歌词识曲/Lyrics_recognition.py
+++ 歌词识曲/Lyrics_recognition.py
@@ -1,52 +1,59 @@
-# 模糊匹配，process模块，字符串与列表中元素的匹配
+# 6. 数据存入json,读取json
 # fuzzywuzzy  process,fuzz模块,process模块比较字符串和列表的，fuzz模块是比较两个字符串的
-# process.extractOne(字符串,列表)，生成一个元组，返回的第一个是列表中最匹配的元素，第二个是分数
+# 1.将目录中的内容放到列表中去
 import os
+import json
 from fuzzywuzzy import process
 from fuzzywuzzy import fuzz
 
 
 def Get_song_keywords(path):
-#遍历path下所有文件
-    files = os.listdir(path)  # 返回指定目录下的所有文件
-    songs = []  # 一个三层嵌套列表，把文件夹下所有的数据存入列表中
-    for file in files:  # 遍历文件
-        file_name = os.path.join(path, file)  # 路径拼接：路径+文件名=绝对路径
-        son = []  # son存的是一个文件的数据，第一个元素是个名字，第二个元素是歌词（歌词是一个列表）
-        with open(file_name, 'r', encoding='utf-8')as f:  # 打开文件
-            song = f.readline()  # 读取第一行，歌名
-            lyric = f.readlines()  # 读取所有的行，歌词列表
-            son.append(song)  # 歌名存入son列表中
-            son.append(lyric)  # 歌词存入son列表中
-            songs.append(son)  # 将son文件存入songs中
-    return songs  # 返回songs列表，列表总包含文件夹下所有文件的数据
+    # 遍历filepath下所有文件，包括子目录
+    files = os.listdir(path)  # 返回指定目录下的所有文件和目录名
+    songs = []
+    for file in files:
+        file_name = os.path.join(path, file)  # 路径拼接
+        son = []
+        with open(file_name, 'r', encoding='utf-8')as f:
+            song = f.readline()
+            lyric = f.readlines()
+            son.append(song)
+            son.append(lyric)
+            songs.append(son)
+    return songs
 
-# 遍历模糊匹配
+
+# 生成一个三层列表，第一层元素是歌，第二层元素是歌名+歌词列表，第三层元素是每一句歌词
+
+
+def json_save(new_file, songs):  # 传入songs列表，输出json文件
+    with open(new_file, 'w', encoding='utf-8') as fp:
+        json.dump(songs, fp, ensure_ascii=False)
+    return new_file
+
+
+def json_read(j):  # 输入json文件，输出
+    with open(j, 'r', encoding='utf8') as fp:
+        json_str = json.load(fp)
+    return json_str
+
+
+# 遍历匹配
 # process.extractOne(字符串,列表)，生成一个元组，返回的第一个是列表中最匹配的元素，第二个是分数
-def search(songs):  # 输入歌词，输出歌名
-    ly = input("请输入歌词：")  # 手动输入歌词
+# AttributeError: ‘str’ object has no attribute ‘load’;解决办法不应将变量名命名为 json。
+def search(songs):
+    ly = input("请输入歌词：")
     match = process.extractOne(ly, songs)
-# 生成一个元组两个元素，第一个元素是列表中最匹配的元素，第二个是对它的打分
-    """
-    (['世界这么大还是遇见你 - 程响', 
-    ['背包塞满青涩的回忆 \n', '就要踏上成长的旅程 \n', '就到 这个路口 \n', '你就不要送我 你快回去 \n', 
-    '相逢又告别一句再见 \n', '过去的一切不会重现 \n', '失落的时候 \n', '请像我一样相信你自己 \n', '世界这么大还是遇见你 \n', 
-    '多少次疯狂 多少天真 \n', '一起做过梦 \n', '有一天我们会重逢故里 \n', '世界这么大还是遇见你 \n', '一起走过许多个四季 \n', 
-    '天南地北 \n', '别忘记我们之间的情谊 \n', '背包塞满青涩的回忆 \n', '就要踏上成长的旅程 \n', '就到 这个路口 \n', '你就不要送我 你快回去 \n', 
-    '相逢又告别一句再见 \n', '过去的一切不会重现 \n', '失落的时候 \n', '请像我一样相信你自己 \n', '世界这么大还是遇见你 \n', '多少次疯狂 多少天真 \n', 
-    '一起做过梦 \n', '有一天我们会重逢故里 \n', '世界这么大还是遇见你 \n', '一起走过许多个四季 \n', '天南地北 \n', '别忘记我们之间的情谊 \n', 
-    '世界这么大还是遇见你 \n', '多少次疯狂 多少天真 \n', '一起做过梦 \n', '有一天我们会重逢故里 \n', '世界这么大还是遇见你 \n',
-     '一起走过许多个四季 \n', '天南地北 \n', '别忘记我们之间的情谊 \n', '世界这么大还是遇见你 \n', '多少次疯狂 多少天真 \n', '一起做过梦 \n',
-      '有一天我们会重逢故里 \n', '世界这么大还是遇见你 \n', '一起走过许多个四季 \n', '天南地北 \n', '别忘记我们之间的情谊 ']], 60)
-    """
-
-    if match[1] >= 50:  # 如果大于50分
-        print(match[0][0])  # 就打印歌名
-    else:  # 否则就打印没有
+    if match[1] >= 50:
+        print(match[0][0])
+    else:
         print('没有匹配的歌词')
 
 
 if __name__ == "__main__":
-    path = 'lyric'  # 歌词文件夹
-    songs = Get_song_keywords(path)  # 将文件夹下的所有文件的数据存入列表
-    search(songs)  # 遍历模糊匹配，输入歌词，输出歌名
+    # 递归遍历目录下所有文件
+    path = r'C:\Users\java\Desktop\python\lyric'
+    songs = Get_song_keywords(path)
+    j = json_save('songs.json', songs)
+    m = json_read(j)
+    search(m)
